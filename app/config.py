472e926785d514be940204cd3fc4ffb32ca4d636
Anshuman Singh <jaspreetsinghaniya64@gmail.com>
2025-04-17 13:40:08 +0530

    Added render.yaml and reportlab

--- app/config.py
+++ app/config.py
@@ -6,9 +6,9 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here'
- #   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     'mysql+pymysql://root:1234@localhost/diesel_delivery'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sarvarsingh:(sarvar777)@db4free.net/diesel_delivery'
+    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sarvarsingh:(sarvar777)@db4free.net/diesel_delivery'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'app/static/uploads'
     INVOICE_FOLDER = os.path.join(basedir, 'static', 'invoices')
