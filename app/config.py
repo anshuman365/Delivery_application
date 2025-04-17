98084c21da2b4704b7860f34f02dac49f18d1c81
Anshuman Singh <jaspreetsinghaniya64@gmail.com>
2025-04-17 15:01:06 +0530

    Updated config.py

--- app/config.py
+++ app/config.py
@@ -6,9 +6,10 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here'
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    'mysql+pymysql://root:1234@localhost/diesel_delivery'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sarvarsingh:(sarvar777)@db4free.net/diesel_delivery'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sql12773739:muGBJitR88@sql12.freesqldatabase.com:3306/sql12773739'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'app/static/uploads'
     INVOICE_FOLDER = os.path.join(basedir, 'static', 'invoices')
+
+
+#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@localhost/diesel_delivery'
